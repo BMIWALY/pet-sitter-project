a2b1b53a667da1860565ef92eb526343cb26bbb9
wanchana <windaywf@@gmail.com>
2024-03-18 21:08:24 +0700

    feat: add landing page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,15 +1,50 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { Button, Stack } from "@mui/material";
+import { Button, Container, Stack, Typography } from "@mui/material";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <Stack sx={{width:"100px",justifyContent:"center"}}>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-    </Stack>
+    <Container maxWidth="lg" sx={{ height: "100vh"}} >
+      <Stack
+        direction="row"
+        sx={{ justifyContent: "center", alignItems: "center" }}
+      >
+        <Stack>
+          <Image
+            src="/images/navbar/Frame 427320930.svg"
+            alt="logo"
+            width={428}
+            height={441}
+          />
+        </Stack>
+        <Stack>
+          <Image
+            src="/images/navbar/Frame 427320929.svg"
+            alt="logo"
+            width={544}
+            height={352}
+          />
+        </Stack>
+        <Stack>
+          <Image
+            src="/images/navbar/Frame 427320931.svg"
+            alt="logo"
+            width={428}
+            height={441}
+          />
+        </Stack>
+      </Stack>
+      <Stack sx={{width:'1064px', height:'168px', backgroundColor:'red',borderRadius:'10px',justifyContent:'center'}}>
+
+      </Stack>
+
+
+      <Typography variant="h4" sx={{fontWeight:700, textAlign:'center'}}> {/* Use variant="h2" */}
+        {"Your Pets, Our Priority: Perfect Care, Anytime, Anywhere."}
+      </Typography>
+
+    </Container>
   );
 }
